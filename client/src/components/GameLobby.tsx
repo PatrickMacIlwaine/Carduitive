c0cb3ac74d8730f54e9fd97e08e554521e2f2e5a
Patrick MacIlwaine <patrick@macilwaine.com>
2024-09-03 11:12:54 -0500

    hooks

--- client/src/components/GameLobby.tsx
+++ client/src/components/GameLobby.tsx
@@ -6,6 +6,7 @@ import WinPage from './WinPage';
 import LossPage from './LossPage';
 import { v4 as uuidv4 } from 'uuid';
 import { useParams } from 'react-router-dom';
+import { useWebSocket } from './hooks/useWebSockets';
 
 export default function GameLobby() {
   const [displayPreStartPage, setDisplayPreStartPage] = useState(true);
@@ -14,100 +15,15 @@ export default function GameLobby() {
   const [displayWinPage, setDisplayWinPage] = useState(false);
   const [displayLossPage, setDisplayLossPage] = useState(false);
 
-  interface GameState {
-    playersConnected: number;
-    playersReady: number;
-    playerCount: number;
-    level: number;
-    win: boolean;
-    loss: boolean;
-    myCards: Array<number>;
-    otherPlayersCards: Array<Array<number>>;
-    lastPlayedCard: number;
-    streamerMode: boolean;
-    timeConstraint: boolean;
-    playedCardsHistory: Array<number>;
-    highScore: number;
-  }
-
-  const [gameState, setGameState] = useState<GameState>({
-    playersConnected: 0,
-    playersReady: 0,
-    playerCount: 99,
-    level: 1,
-    win: false,
-    loss: false,
-    myCards: [1, 2, 3],
-    otherPlayersCards: [],
-    lastPlayedCard: 0,
-    streamerMode: false,
-    timeConstraint: false,
-    playedCardsHistory: [],
-    highScore: 1,
-  });
-
   const SERVER_URL = 'ws://localhost:8080';
-  const [clientId, setClientId] = useState<string>('');
-  const [ws, setWs] = useState<WebSocket | null>(null);
   const { lobbyCode } = useParams();
+  const clientId = uuidv4();
 
-  useEffect(() => {
-    if (!lobbyCode) return;
-
-    const savedClientId = localStorage.getItem('clientId') || uuidv4();
-    localStorage.setItem('clientId', savedClientId);
-    setClientId(savedClientId);
-
-    const socket = new WebSocket(
-      `${SERVER_URL}?lobbyCode=${lobbyCode}&clientId=${savedClientId}`
-    );
-
-    socket.onopen = () => console.log('WebSocket connection established');
-    socket.onerror = (error) => console.log('WebSocket error:', error);
-    socket.onmessage = (event) => {
-      console.log('Message from server:', event.data);
-      const message = JSON.parse(event.data);
-
-      if (message.type === 'lobbyUpdate' || 'fullStateUpdate') {
-        console.log('Received lobby update:', message.payload);
-
-        setGameState((prevState) => ({
-          ...prevState,
-          playersConnected:
-            message.payload.playersConnected ?? prevState.playersConnected,
-          playersReady: message.payload.playersReady ?? prevState.playersReady,
-          playerCount: message.payload.playerCount ?? prevState.playerCount,
-          level: message.payload.level ?? prevState.level,
-          win: message.payload.win ?? prevState.win,
-          loss: message.payload.loss ?? prevState.loss,
-          myCards: message.payload.myCards ?? prevState.myCards,
-          otherPlayersCards:
-            message.payload.otherPlayersCards ?? prevState.otherPlayersCards,
-          lastPlayedCard:
-            message.payload.lastPlayedCard ?? prevState.lastPlayedCard,
-          streamerMode: message.payload.streamerMode ?? prevState.streamerMode,
-          timeConstraint:
-            message.payload.timeConstraint ?? prevState.timeConstraint,
-          playedCardsHistory:
-            message.payload.playedCardsHistory ?? prevState.playedCardsHistory,
-          highScore: message.payload.highScore ?? prevState.highScore,
-        }));
-      }
-    };
-
-    setWs(socket);
-
-    return () => {
-      socket.close();
-      console.log('WebSocket closed');
-    };
-  }, [lobbyCode]);
-
-  const sendMessage = (message: string) => {
-    if (ws && ws.readyState === WebSocket.OPEN) {
-      ws.send(message);
-    }
-  };
+  const { ws, gameState, sendMessage } = useWebSocket(
+    lobbyCode as string,
+    clientId,
+    SERVER_URL
+  );
 
   useEffect(() => {
     if (gameState.loss) {
@@ -162,10 +78,20 @@ export default function GameLobby() {
         />
       )}
       {displayWinPage && !displayLossPage && (
-        <WinPage highScore={ gameState.highScore } ws={ws} lobbyCode={lobbyCode} sendMessage={sendMessage} />
+        <WinPage
+          highScore={gameState.highScore}
+          ws={ws}
+          lobbyCode={lobbyCode}
+          sendMessage={sendMessage}
+        />
       )}
       {displayLossPage && !displayWinPage && (
-        <LossPage highScore={ gameState.highScore } ws={ws} lobbyCode={lobbyCode} sendMessage={sendMessage} />
+        <LossPage
+          highScore={gameState.highScore}
+          ws={ws}
+          lobbyCode={lobbyCode}
+          sendMessage={sendMessage}
+        />
       )}
     </div>
   );
---
+++ client/src/components/hooks/useWebSockets.tsx
@@ -0,0 +1,101 @@
+import { useEffect, useState } from 'react';
+
+interface GameState {
+  playersConnected: number;
+  playersReady: number;
+  playerCount: number;
+  level: number;
+  win: boolean;
+  loss: boolean;
+  myCards: Array<number>;
+  otherPlayersCards: Array<Array<number>>;
+  lastPlayedCard: number;
+  streamerMode: boolean;
+  timeConstraint: boolean;
+  playedCardsHistory: Array<number>;
+  highScore: number;
+}
+
+const initialGameState: GameState = {
+  playersConnected: 0,
+  playersReady: 0,
+  playerCount: 99,
+  level: 1,
+  win: false,
+  loss: false,
+  myCards: [],
+  otherPlayersCards: [],
+  lastPlayedCard: 0,
+  streamerMode: false,
+  timeConstraint: false,
+  playedCardsHistory: [],
+  highScore: 1,
+};
+
+export const useWebSocket = (
+  lobbyCode: string,
+  clientId: string,
+  SERVER_URL: string
+) => {
+  const [ws, setWs] = useState<WebSocket | null>(null);
+  const [gameState, setGameState] = useState<GameState>(initialGameState);
+
+  useEffect(() => {
+    if (!lobbyCode) return;
+
+    const savedClientId = localStorage.getItem('clientId') || clientId;
+    localStorage.setItem('clientId', savedClientId);
+
+    const socket = new WebSocket(
+      `${SERVER_URL}?lobbyCode=${lobbyCode}&clientId=${savedClientId}`
+    );
+
+    socket.onopen = () => console.log('WebSocket connection established');
+    socket.onerror = (error) => console.log('WebSocket error:', error);
+    socket.onmessage = (event) => {
+      console.log('Message from server:', event.data);
+      const message = JSON.parse(event.data);
+
+      if (message.type === 'lobbyUpdate' || 'fullStateUpdate') {
+        console.log('Received lobby update:', message.payload);
+
+        setGameState((prevState) => ({
+          ...prevState,
+          playersConnected:
+            message.payload.playersConnected ?? prevState.playersConnected,
+          playersReady: message.payload.playersReady ?? prevState.playersReady,
+          playerCount: message.payload.playerCount ?? prevState.playerCount,
+          level: message.payload.level ?? prevState.level,
+          win: message.payload.win ?? prevState.win,
+          loss: message.payload.loss ?? prevState.loss,
+          myCards: message.payload.myCards ?? prevState.myCards,
+          otherPlayersCards:
+            message.payload.otherPlayersCards ?? prevState.otherPlayersCards,
+          lastPlayedCard:
+            message.payload.lastPlayedCard ?? prevState.lastPlayedCard,
+          streamerMode: message.payload.streamerMode ?? prevState.streamerMode,
+          timeConstraint:
+            message.payload.timeConstraint ?? prevState.timeConstraint,
+          playedCardsHistory:
+            message.payload.playedCardsHistory ?? prevState.playedCardsHistory,
+          highScore: message.payload.highScore ?? prevState.highScore,
+        }));
+      }
+    };
+
+    setWs(socket);
+
+    return () => {
+      socket.close();
+      console.log('WebSocket closed');
+    };
+  }, [lobbyCode]);
+
+  const sendMessage = (message: string) => {
+    if (ws && ws.readyState === WebSocket.OPEN) {
+      ws.send(message);
+    }
+  };
+
+  return { ws, gameState, sendMessage };
+};
